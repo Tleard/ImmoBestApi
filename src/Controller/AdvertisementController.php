b044a77a507a66462735f4174a22f5ada5f6643a
Thomas <huxtra@gmail.com>
2020-02-19 13:28:45 +0100

    Create Method for advertisement

--- src/Controller/AdvertisementController.php
+++ src/Controller/AdvertisementController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Advertisement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 /**
  * Class AdvertisementController
  * @package App\Controller
@@ -32,8 +37,30 @@ class AdvertisementController extends AbstractController {
     ]
     ];
 
+
     /**
-     * @Route("/{page}", name="AdvertisementList", defaults={"page": 1})
+     * @param Request $request
+     * @Route("/add", name="blog_add", methods={"POST"})
+     * @return JsonResponse
+     */
+    public function add(Request $request)
+    {
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+        
+        $advertisement = $serializer->deserialize($request->getContent(), Advertisement::class, 'json');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($advertisement);
+        $em->flush();
+
+        return $this->json($advertisement);
+    }
+
+    /**
+     * @Route("/{page}", name="AdvertisementList", defaults={"page": 1}, requirements={"page"="\d+"})
      * @param $page
      * @return JsonResponse
      */
@@ -43,7 +70,7 @@ class AdvertisementController extends AbstractController {
             [
                 'page' => $page,
                 'data' => array_map(function ($item){
-                    return $this->generateUrl('blog_by_id', ['id' =>$item['id']]);
+                    return $this->generateUrl('AdvertisementPost', ['id' =>$item['id']]);
                 }, self::POSTS)
             ]
         );
---
+++ src/Entity/Advertisement.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AdvertisementRepository")
+ */
+class Advertisement
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    //Todo: Change for Datetime Property
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $published;
+
+    /**
+     * @ORM\Column(type="text", length=255)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    public function setPublished($published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug): void
+    {
+        $this->slug = $slug;
+    }
+
+
+}
