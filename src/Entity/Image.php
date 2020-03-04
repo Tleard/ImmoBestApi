707f1085d0c2198e6d2b72db824e1839c771f3c2
Thomas <thomas.leard@gmail.com>
2020-03-04 11:52:29 +0100

    Create Image Entity

---
+++ src/Entity/Image.php
@@ -0,0 +1,73 @@
+<?php
+
+
+namespace App\Entity;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * Class Image
+ * @package App\Entity
+ * @ORM\Entity()
+ * @Vich\Uploadable()
+ * @ApiResource(
+ *
+ * )
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Vich\UploadableField(mapping="images", fileNameProperty="url")
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    private $url;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function setFile($file): void
+    {
+        $this->file = $file;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url): void
+    {
+        $this->url = $url;
+    }
+
+}
